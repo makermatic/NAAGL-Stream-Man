6517d4f62b0b82eba7470c3b41a1662be79fe243
makermatic <44180512+makermatic@users.noreply.github.com>
2021-08-07 11:56:02 -0400

    UI refresh
    Teams and Score are consolidated into one dashboard panel, with three buttons

--- bundles/naggl_bundle/dashboard/score.js
+++ bundles/naggl_bundle/dashboard/score.js
@@ -4,32 +4,37 @@ const team2Rep = nodecg.Replicant('team2', { defaultValue: 0 });
 var swapTog = !true;
 
 function addTeam1() {
-	team1Rep.value++;
+    team1Rep.value++;
 }
 
 function addTeam2() {
-	team2Rep.value++;
+    team2Rep.value++;
 }
 
 function subtractTeam1() {
-	team1Rep.value--;
+    team1Rep.value--;
 }
 
 function subtractTeam2() {
-	team2Rep.value--;
+    team2Rep.value--;
 }
 
 function resetScores() {
-	team1Rep.value = 0;
-	team2Rep.value = 0;
+    team1Rep.value = 0;
+    team2Rep.value = 0;
 }
 
 function swapScores() {
-	team1Rep.value = team2Rep.value;
-	team2Rep.value = team1Rep.value;
+    team1Rep.value = team2Rep.value;
+    team2Rep.value = team1Rep.value;
 }
 
-checkbox.addEventListener('change', function () {
-	data = {value: checkbox.checked}
-	nodecg.sendMessage('scoresoffToggle', data)
+function resetScore() {
+    team1Rep.value = 0;
+    team2Rep.value = 0;
+}
+
+checkbox.addEventListener('change', function() {
+    data = { value: checkbox.checked }
+    nodecg.sendMessage('scoresoffToggle', data)
 })
--- bundles/naggl_bundle/dashboard/teams.js
+++ bundles/naggl_bundle/dashboard/teams.js
@@ -4,17 +4,17 @@ const team2Name = document.getElementById('team2Name')
 var swapTog = !true;
 
 function update() {
-	const data = { team1Name: team1Name.value, team2Name: team2Name.value }
-	nodecg.sendMessage('updateTeams', data)
+    const data = { team1Name: team1Name.value, team2Name: team2Name.value }
+    nodecg.sendMessage('updateTeams', data)
 }
 
-function swap() {
-	if (swapTog != true) {
-		const data = { team1Name: team1Name.value, team2Name: team2Name.value }
-		nodecg.sendMessage('swapTeams', data)
-		swapTog = true;
-	} else {
-		update()
-		swapTog = false;
-	}
+function swapTeams() {
+    if (swapTog != true) {
+        const data = { team1Name: team1Name.value, team2Name: team2Name.value }
+        nodecg.sendMessage('swapTeams', data)
+        swapTog = true;
+    } else {
+        update()
+        swapTog = false;
+    }
 }
