d7285ffdfa8e1076d2629197a39a61a7b14a1886
makermatic <makermatic@users.noreply.github.com>
2021-08-10 18:29:10 -0400

    New In Game Layout woo

--- bundles/naggl_bundle/dashboard/score.js
+++ bundles/naggl_bundle/dashboard/score.js
@@ -1,8 +1,6 @@
 const team1Rep = nodecg.Replicant('team1', { defaultValue: 0 });
 const team2Rep = nodecg.Replicant('team2', { defaultValue: 0 });
 
-var swapTog = !true;
-
 function addTeam1() {
     team1Rep.value++;
 }
@@ -17,11 +15,6 @@ function subtractTeam1() {
 
 function subtractTeam2() {
     team2Rep.value--;
-}
-
-function resetScores() {
-    team1Rep.value = 0;
-    team2Rep.value = 0;
 }
 
 function swapScores() {
